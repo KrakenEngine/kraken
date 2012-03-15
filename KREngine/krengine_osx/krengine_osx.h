a9786b80e68caf99c9b424af57b2ec4019ce21b5
kearwood <none@none>
2012-03-15 22:34:08 +0000

    Created target for KREngine to compile as a framework for OSX.
    --HG--
    extra : convert_revision : svn%3A7752d6cf-9f14-4ad2-affc-04f1e67b81a5/trunk%4014

---
+++ KREngine/krengine_osx/krengine_osx.h
@@ -0,0 +1,13 @@
+//
+//  krengine_osx.h
+//  krengine_osx
+//
+//  Created by Kearwood Gilbert on 12-03-15.
+//  Copyright (c) 2012 Kearwood Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface krengine_osx : NSObject
+
+@end
---
+++ KREngine/krengine_osx/krengine_osx.m
@@ -0,0 +1,13 @@
+//
+//  krengine_osx.m
+//  krengine_osx
+//
+//  Created by Kearwood Gilbert on 12-03-15.
+//  Copyright (c) 2012 Kearwood Software. All rights reserved.
+//
+
+#import "krengine_osx.h"
+
+@implementation krengine_osx
+
+@end
