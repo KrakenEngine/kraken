ecc233ddb30f1a842e86d7487f16dcb110f6b4f7
kearwood <none@none>
2012-03-15 17:15:28 +0000

    First commit of the KRWorldBuilder project.  Nothing functional yet
    --HG--
    extra : convert_revision : svn%3A7752d6cf-9f14-4ad2-affc-04f1e67b81a5/trunk%407

---
+++ KRWorldBuilder/KRWorldBuilder/KRWBDocument.h
@@ -0,0 +1,13 @@
+//
+//  KRWBDocument.h
+//  KRWorldBuilder
+//
+//  Created by Kearwood Gilbert on 12-02-10.
+//  Copyright (c) 2012 Kearwood Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KRWBDocument : NSDocument
+
+@end
---
+++ KRWorldBuilder/KRWorldBuilder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KRWorldBuilder
+//
+//  Created by Kearwood Gilbert on 12-02-10.
+//  Copyright (c) 2012 Kearwood Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
