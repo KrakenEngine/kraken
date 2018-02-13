4f32fb26f450fb25a05a733c44bd8524a97f4fd5
Kearwood Gilbert <kearwood@kearwood.com>
2018-02-13 10:42:03 -0800

    Updated license in public headers

--- kraken/public/scalar.h
+++ kraken/public/scalar.h
@@ -2,7 +2,7 @@
 //  KRFloat.h
 //  Kraken
 //
-//  Copyright 2017 Kearwood Gilbert. All rights reserved.
+//  Copyright 2018 Kearwood Gilbert. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are
 //  permitted provided that the following conditions are met:
