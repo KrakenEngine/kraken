0f8feb669cea4d081abe42adfc5433a1f5b2b6b6
kearwood <kearwood@kearwood.com>
2022-07-02 18:31:48 -0700

    Fix Azure CI, MSVC CMake integration.  Update configure batch script to VS 2022

--- configure_win64_msvc.bat
+++ configure_win64_msvc.bat
@@ -1,2 +1,2 @@
 rem call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvars64.bat"
-cmake -H. -G"Visual Studio 16 2019" -Bbuild -DCMAKE_TOOLCHAIN_FILE="toolchain/toolchain-x86_64-pc-windows-msvc.cmake"
+cmake -H. -G"Visual Studio 17 2022" -Bbuild -DCMAKE_TOOLCHAIN_FILE="toolchain/toolchain-x86_64-pc-windows-msvc.cmake"
