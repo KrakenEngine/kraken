8845e19273c994a7e05ea7b9a578bd4b828476db
Kearwood Gilbert <kearwood@kearwood.com>
2023-01-10 14:22:26 -0800

    Bump copyright year.

--- kraken/KRAnimationCurve.cpp
+++ kraken/KRAnimationCurve.cpp
@@ -2,7 +2,7 @@
 //  KRAnimationCurve.cpp
 //  Kraken Engine
 //
-//  Copyright 2022 Kearwood Gilbert. All rights reserved.
+//  Copyright 2023 Kearwood Gilbert. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are
 //  permitted provided that the following conditions are met:
