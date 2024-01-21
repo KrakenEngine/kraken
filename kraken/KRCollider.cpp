2925ea316d12784b0061c237da71ae4cdf2c25d7
Kearwood Gilbert <kearwood@kearwood.com>
2024-01-20 19:01:29 -0800

    Bump copyright year Update 1st party submodules

--- kraken/KRCollider.cpp
+++ kraken/KRCollider.cpp
@@ -2,7 +2,7 @@
 //  KRCollider.cpp
 //  Kraken Engine
 //
-//  Copyright 2023 Kearwood Gilbert. All rights reserved.
+//  Copyright 2024 Kearwood Gilbert. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are
 //  permitted provided that the following conditions are met:
